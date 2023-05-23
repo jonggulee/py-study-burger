fa4b4c1bd239960c30e357d5de79f4a879ab9ad8
jonggulee <jg.jake.lee@gmail.com>
2023-05-23 18:04:33 +0900

    Get data from view function

--- config/views.py
+++ config/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     return render(request, "main.html")
 
 def burger_list(request):
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록:", burgers)
     return render(request, "burger_list.html")
